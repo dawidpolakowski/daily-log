6bc6d03ff52b1d85c01b5a701b17bd61ae0d09e6
dawidpolakowski <dawidpolakowski@gmail.com>
2026-03-23 11:20:34 +0000

    links update

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 async function loadLogs() {
-  const res = await fetch('logs/logs.json');
-  const logs = await res.json();
+    const res = await fetch('logs/logs.json');
+    const logs = await res.json();
 
-  const container = document.getElementById('logs');
+    const container = document.getElementById('logs');
 
-  logs.reverse().forEach(log => {
-    const el = document.createElement('div');
-    el.innerHTML = `
-      <a href="logs/${log.file}" target="_blank">
-        ${log.title}
-      </a>
-    `;
-    container.appendChild(el);
-  });
+    logs.reverse().forEach(log => {
+        const el = document.createElement('div');
+        el.innerHTML = `
+  <a href="log.html?file=${log.file}">
+    ${log.title}
+  </a>
+`;
+        container.appendChild(el);
+    });
 }
 
 loadLogs();
